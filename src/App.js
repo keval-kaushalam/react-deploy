2a182222332645d682fd679e8a06e75ffa09a145
Keval Takodara <keval@kaushalaminc.com>
2021-09-16 15:04:25 +0530

    second test commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My First Deploy.
-        </p>
+        <h1>
+          Hello kem cho???? Mja ma ne palty...... 
+        </h1>
       </header>
     </div>
   );
